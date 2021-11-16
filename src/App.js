57ba6e6b780658fc5b9b41a1d4e63ea6ddf5969b
Aaron Donelson <aaron.donelson23@gmail.com>
2021-11-16 18:25:57 -0500

    added api key

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import MovieList from './components/MovieList';
+
+const App = () => {
+	const [movies, setMovies] = useState([]);
+
+	const getMovieRequest = async () => {
+		const url = `http://www.omdbapi.com/?i=tt3896198&apikey=b8d3ecea`;
+
+		const response = await fetch(url);
+		const responseJson = await response.json();
+
+		if (responseJson.Search) {
+			setMovies(responseJson.Search);
+		}
+	};
+
+	useEffect(() => {
+		getMovieRequest();
+	}, []);
+
+	return (
+		<div className='container-fluid movie-app'>
+			<div className='row'>
+				<MovieList movies={movies} />
+			</div>
+		</div>
+	);
+};
+
+export default App;
