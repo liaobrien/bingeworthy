8ae3acdc21878b08c3d4e4020b147c19b4c61eb0
Aaron Donelson <aaron.donelson23@gmail.com>
2021-11-17 10:09:13 -0500

    search box working

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React, { useState, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import MovieList from './components/MovieList';
+import MovieHeading from './components/MovieHeading';
+import SearchBox from './components/SearchBox';
 
 const App = () => {
 	const [movies, setMovies] = useState([]);
+	const [searchValue, setSearchValue] = useState( '' );
 
 	const getMovieRequest = async () => {
-		const url = `http://www.omdbapi.com/?s=star wars&apikey=b8d3ecea`;
+		const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=b8d3ecea`;
 
 		const response = await fetch(url);
 		const responseJson = await response.json();
@@ -18,11 +21,15 @@ const App = () => {
 	};
 
 	useEffect(() => {
-		getMovieRequest();
-	}, []);
+		getMovieRequest(searchValue);
+	}, [searchValue]);
 
 	return (
 		<div className='container-fluid movie-app'>
+			<div className='row d-flex align-items-center mt-4 mb-4'>
+				<MovieHeading heading='Movies' />
+				<SearchBox searchValue={searchValue} setSearchValue={setSearchValue} />
+			</div>
 			<div className='row'>
 				<MovieList movies={movies} />
 			</div>
