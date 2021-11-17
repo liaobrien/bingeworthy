b18b42f71f4770d02d20838d664e8147acd99dff
Mark Cirineo <88806942+MarkCirineo@users.noreply.github.com>
2021-11-17 11:41:16 -0500

    created signToken method to login a user

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -23,5 +23,10 @@ module.exports = {
             console.log("Invalid token");
         }
         return req;
-    }
-}
+    },
+    signToken: function({ username, email, _id }) {
+        const payload = { username, email, _id };
+
+        return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+    },
+};
