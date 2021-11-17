ef72e78364742e0587a8a9a14ce1ac3162fbe163
Lia OBrien <liaobrien123@gmail.com>
2021-11-17 12:50:51 -0500

    filled out mutations.js

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,101 @@
+import { gql } from '@apollo/client';
+
+export const LOGIN_USER = gql`
+      mutation login($email: String!, $password: String!) {
+            login(email: $email, password: $password) {
+                  token
+                  user {
+                        _id
+                        username
+                  }
+            }
+      }
+`;
+
+export const ADD_USER = gql`
+      mutation addUser($username: String!, $email: String!, $password: String!) {
+            addUser(username: $username, email: $email, password: $password) {
+                  token
+                  user {
+                        _id
+                        username
+                  }
+            }
+      }
+`;
+
+export const DELETE_USER = gql`
+      mutation deleteUser($id: ID!) {
+            deleteUser(id: $id) {
+                  token
+                  user {
+                        _id
+                        username
+                  }
+            }
+      }
+`;
+
+export const ADD_LIST = gql`
+      mutation addList($name: String!) {
+            addList(name: $name) {
+                  _id
+                  username
+                  email
+                  listCount
+                  lists {
+                        _id
+                        name
+                  }
+            }
+      }
+`;
+
+export const DELETE_LIST = gql`
+      mutation deleteList($id: ID!) {
+            deleteList(id: $id) {
+                  _id
+                  username
+                  email
+                  listCount
+                  lists {
+                        _id
+                        name
+                  }
+            }
+      }
+`;
+
+export const ADD_MOVIE = gql`
+      mutation addMovie(listID: ID!, imdbID: String!) {
+            addMovie(listID: $listID, imdbID: $imdbID) {
+                  _id
+                  name
+                  movies {
+                        imdbID
+                        title
+                        releaseDate
+                        poster
+                        plot
+                        watched
+                  }
+            }
+      }
+`;
+
+export const DELETE_MOVIE = gql`
+      mutation deleteMovie(listID: ID!, imdbID: String!) {
+            deleteMovie(listID: $listID, imdbID: $imdbID) {
+                  _id
+                  name
+                  movies {
+                        imdbID
+                        title
+                        releaseDate
+                        poster
+                        plot
+                        watched
+                  }
+            }
+      }
+`;
