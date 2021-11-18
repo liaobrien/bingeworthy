b661a6dec1ee65d3658b397634b02c9cc937a783
Aaron Donelson <aaron.donelson23@gmail.com>
2021-11-18 14:15:37 -0500

    moved search feature to only be on home page. adjusted some styles.

--- client/src/components/WatchList.js
+++ client/src/components/WatchList.js
@@ -1,26 +1,26 @@
-// import React, { useState } from "react";
-// import 'bootstrap/dist/css/bootstrap.min.css';
-// import './App.css';
-// import MovieList from "./components/MovieList";
-// import MovieHeading from "./components/MovieHeading";
-// import AddToWatchList from "./components/AddToWatchList";
+import React, { useState } from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import MovieList from "./components/MovieList";
+import MovieHeading from "./components/MovieHeading";
+import AddToWatchList from "./components/AddToWatchList";
 
-// const ViewMovie = () => {
+const ViewMovieList = () => {
 
-// 	const [RemoveFromWatch, setRemoveFromWatch] = useState([]);
+	const [RemoveFromWatch, setRemoveFromWatch] = useState([]);
 
-//   return (
-//     <div className="container-fluid movie-app">
-//       <div className="row d-flex align-items-center mt-4 mb-4">
-//         <MovieHeading heading="Your BingeWorthy Movies" />
-//       </div>
-//       <div className="row d-flex align-items-center mt-4 mb-4"></div>
-//       <div className="row">
-//         <MovieList movies={AddToWatch} watchListComponent={AddToWatchList} /> /* need to remove AddTo Watch and AddToWatchList and replace with Remove Function */
-//         /* also need to add a view button and view button fuctionality */
-//       </div>
-//     </div>
-//   );
-// };
+  return (
+    <div className="container-fluid movie-app">
+      <div className="row d-flex align-items-center mt-4 mb-4">
+        <MovieHeading heading="Your BingeWorthy Movies" />
+      </div>
+      <div className="row d-flex align-items-center mt-4 mb-4"></div>
+      <div className="row">
+        <MovieList movies={AddToWatch} watchListComponent={AddToWatchList} /> /* need to remove AddTo Watch and AddToWatchList and replace with Remove Function */
+        /* also need to add a view button and view button fuctionality */
+      </div>
+    </div>
+  );
+};
 
-// export default ViewMovie;
+export default ViewMovieList;
