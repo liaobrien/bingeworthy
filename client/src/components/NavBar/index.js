91eca48fb12daa56cd864e6690865eec51c62361
Mark Cirineo <88806942+MarkCirineo@users.noreply.github.com>
2021-11-18 20:42:08 -0500

    added conditional rendering for the login/signup and logout buttons

--- client/src/components/NavBar/index.js
+++ client/src/components/NavBar/index.js
@@ -3,6 +3,7 @@ import React from "react";
 import { Link } from "react-router-dom"
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
+import Auth from '../../utils/auth';
 
 
 
@@ -13,12 +14,22 @@ export default function Navigation() {
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="me-auto">
-                    <Link className="nav-item nav-link" to="/">Home</Link>
-                    <Link className="nav-item nav-link" to="/BingeWorthy"> BingeWorthy </Link>
-                    <Link className="nav-item nav-link" to="/Donate">Donate</Link>
-
+                    <Link className="nav-item nav-link text-white" to="/">Home</Link>
+                    <Link className="nav-item nav-link text-white" to="/BingeWorthy"> BingeWorthy </Link>
+                    <Link className="nav-item nav-link text-white" to="/Donate">Donate</Link>
+                    {Auth.loggedIn() ? (
+                        <>
+                            <Nav.Link className="nav-item nav-link text-white" as={Link} to="/lists">
+                                See your lists
+                            </Nav.Link>
+                            <Nav.Link className="nav-item nav-link text-white" onClick={Auth.logout}>Logout</Nav.Link>
+                        </>
+                    ) : (
+                        <Nav.Link className="nav-item nav-link text-white" as={Link} to="/login">Login/Sign Up</Nav.Link>
+                        // <Nav.Link className="nav-item nav-link text-white" onClick={() => setShowModal(true)}>Login/Sign Up</Nav.Link>
+                    )}
                 </Nav>
             </Navbar.Collapse>
-        </Navbar >
+        </Navbar>
     )
 }
