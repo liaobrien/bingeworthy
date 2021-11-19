d94d101b420b1e1a04191e105d16aa00209fd47e
Mark Cirineo <88806942+MarkCirineo@users.noreply.github.com>
2021-11-18 20:36:41 -0500

    changed the name of the class for the movie search

--- client/src/components/SearchBox/SearchBox.js
+++ client/src/components/SearchBox/SearchBox.js
@@ -4,7 +4,7 @@ const SearchBox = (props) => {
     return (
         <div className= 'col col-sm-4'>
             <input
-            className = 'form-control'
+            className = 'form-movie-search'
             value={props.value}
             onChange={(event) => props.setSearchValue(event.target.value)}
             placeholder= 'Search your movie here'
