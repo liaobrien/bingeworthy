bd3bcdda070917138c069517b6043af34c974321
Mark Cirineo <88806942+MarkCirineo@users.noreply.github.com>
2021-11-20 20:58:47 -0500

    changed paths on me query

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -3,124 +3,130 @@ const { User, List } = require("../models");
 const { signToken } = require("../utils/auth");
 
 const resolvers = {
-    Query: {
-        me: async (parent, _, context) => {
-            if (context.user) {
-                return User.findOne({ _id: context.user._id }).populate("Lists");
-            }
-            throw new AuthenticationError("You must be logged in!");
-        },
-        list: async (parent, { _id }, context) => {
-            if (context.user) {
-                return List.findOne({ _id }).populate("movies");
-            }
-            throw new AuthenticationError("You must be logged in!")
-        }
+  Query: {
+    me: async (parent, _, context) => {
+      if (context.user) {
+        const user = User.findOne({ _id: context.user._id }).populate({path: "lists"}).populate({path: "movies"});
+        console.log(user);
+        return user;
+      }
+      throw new AuthenticationError("You must be logged in!");
     },
-    Mutation: {
-        addUser: async (parent, { username, email, password }) => {
-            console.log(username);
-            const user = await User.create({ username, email, password });
-            const token = signToken(user);
-            return { token, user };
-        },
-        login: async (parent, { email, password }) => {
-            const user = await User.findOne({ email });
+    list: async (parent, { _id }, context) => {
+      if (context.user) {
+        const list = List.findOne({ _id }).populate({path: "movies"});
+        return list;
+      }
+      throw new AuthenticationError("You must be logged in!");
+    },
+  },
+  Mutation: {
+    addUser: async (parent, { username, email, password }) => {
+      console.log(username);
+      const user = await User.create({ username, email, password });
+      const token = signToken(user);
+      return { token, user };
+    },
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
 
-            if (!user) {
-                throw new AuthenticationError("No user found with this email address");
-            }
+      if (!user) {
+        throw new AuthenticationError("No user found with this email address");
+      }
 
-            const correctPw = await user.isCorrectPassword(password);
+      const correctPw = await user.isCorrectPassword(password);
 
-            if (!correctPw) {
-                throw new AuthenticationError("Incorrect credentials");
-            }
+      if (!correctPw) {
+        throw new AuthenticationError("Incorrect credentials");
+      }
 
-            const token = signToken(user);
+      const token = signToken(user);
 
-            return { token, user };
-        },
-        deleteUser: async (parent, _, context) => {
-            if (context.user) {
-                return User.findOneAndDelete({ _id: context.user._id });
-            }
-            throw new AuthenticationError("You must be logged in")
-        },
-        addList: async (parent, { name }, context) => {
-            if (context.user) {
-                const list = await List.create({ name })
+      return { token, user };
+    },
+    deleteUser: async (parent, _, context) => {
+      if (context.user) {
+        return User.findOneAndDelete({ _id: context.user._id });
+      }
+      throw new AuthenticationError("You must be logged in");
+    },
+    addList: async (parent, { name }, context) => {
+      if (context.user) {
+        const list = await List.create({ name });
 
-                const updatedUser = await User.findOneAndUpdate(
-                    { _id: context.user._id },
-                    {
-                        $addToSet: {
-                            lists: list.id,
-                        },
-                    },
-                    {
-                        new: true,
-                        runValidators: true
-                    }
-                );
-                return updatedUser;
-            }
-            throw new AuthenticationError("You need to be logged in!");
-        },
-        deleteList: async (parent, { id }, context) => {
-            if (context.user) {
-                const list = await List.findOneAndDelete({ _id: id });
-                const updatedUser = await User.findOneAndUpdate(
-                    { _id: context.user._id },
-                    {
-                        $pull: {
-                            lists: list._id
-                        },
-                    },
-                    { new: true }
-                );
-                return updatedUser;
-            }
-            throw new AuthenticationError("You need to be logged in!");
-        },
-        addMovie: async (parent, { movieInput }, context) => {
-            if (context.user ) {
-                const newList = await List.create({ });
-                console.log(newList)
-                await User.findOneAndUpdate({_id: context.user._id}, {$push:{Lists:newList._id}})
-                const listID = newList._id;
-                const updatedList = await List.findOneAndUpdate(
-                    { _id: listID },
-                    {
-                        $addToSet: {
-                            movies: movieInput
-                        },
-                    },
-                    {
-                        new: true,
-                        runValidators: true
-                    }
-                );
-                return updatedList;
-            }
-            throw new AuthenticationError("You need to be logged in!");
-        },
-        deleteMovie: async (parent, { listID, imdbID }, context) => {
-            if (context.user) {
-                const updatedList = await List.findOneAndUpdate(
-                    { _id: listID },
-                    {
-                        $pull: {
-                            movies: { imdbID }
-                        },
-                    },
-                    { new: true }
-                );
-                return updatedList;
-            }
-            throw new AuthenticationError("You need to be logged in!");
-        }
-    }
+        const updatedUser = await User.findOneAndUpdate(
+          { _id: context.user._id },
+          {
+            $addToSet: {
+              lists: list.id,
+            },
+          },
+          {
+            new: true,
+            runValidators: true,
+          }
+        );
+        return updatedUser;
+      }
+      throw new AuthenticationError("You need to be logged in!");
+    },
+    deleteList: async (parent, { id }, context) => {
+      if (context.user) {
+        const list = await List.findOneAndDelete({ _id: id });
+        const updatedUser = await User.findOneAndUpdate(
+          { _id: context.user._id },
+          {
+            $pull: {
+              lists: list._id,
+            },
+          },
+          { new: true }
+        );
+        return updatedUser;
+      }
+      throw new AuthenticationError("You need to be logged in!");
+    },
+    addMovie: async (parent, { movieInput }, context) => {
+      if (context.user) {
+        const newList = await List.create({});
+        console.log(newList);
+        await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $push: { lists: newList._id } }
+        );
+        const listID = newList._id;
+        const updatedList = await List.findOneAndUpdate(
+          { _id: listID },
+          {
+            $addToSet: {
+              movies: movieInput,
+            },
+          },
+          {
+            new: true,
+            runValidators: true,
+          }
+        );
+        return updatedList;
+      }
+      throw new AuthenticationError("You need to be logged in!");
+    },
+    deleteMovie: async (parent, { listID, imdbID }, context) => {
+      if (context.user) {
+        const updatedList = await List.findOneAndUpdate(
+          { _id: listID },
+          {
+            $pull: {
+              movies: { imdbID },
+            },
+          },
+          { new: true }
+        );
+        return updatedList;
+      }
+      throw new AuthenticationError("You need to be logged in!");
+    },
+  },
 };
 
 module.exports = resolvers;
