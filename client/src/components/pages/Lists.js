93e96fe80e168581bc9b6d6996c72317f77acb12
Mark Cirineo <88806942+MarkCirineo@users.noreply.github.com>
2021-11-21 13:39:50 -0500

    added remove movie button and view movie button and edited styling

--- client/src/components/pages/Lists.js
+++ client/src/components/pages/Lists.js
@@ -1,34 +1,20 @@
-import { useQuery } from '@apollo/client';
+import { useMutation, useQuery } from '@apollo/client';
 import React, { useEffect, useState } from 'react';
-import { GET_ME, GET_LIST } from "../../utils/queries";
+import { GET_ME } from "../../utils/queries";
+import { DELETE_LIST } from "../../utils/mutations";
 import Auth from '../../utils/auth';
-import { Jumbotron, Container, CardColumns, Card, Button } from 'react-bootstrap';
-
-const Movies = (props) => {
-    const { data, error, loading } = useQuery(GET_LIST, {
-        variables: {
-            id: props.id
-        }
-    })
-
-    return 
-}
+import { Jumbotron, Container, Row, Card, Button } from 'react-bootstrap';
+import { Link } from "react-router-dom";
+import "./list.css";
 
 const Lists = () => {
     const [userData, setUserData] = useState([]);
     const { loading, data } = useQuery(GET_ME);
-    console.log(data);
     const savedLists = data?.me.lists || [];
-
-    // const listArray = userData.map((list) => {
-    //     return list._id;
-    // });
-    // console.log(listArray);
-
+    const [removeList, { error }] = useMutation(DELETE_LIST)
     
     useEffect(() => {
         const getUserData = async () => {
-    
 
             try {
                 const token = Auth.loggedIn() ? Auth.getToken() : null;
@@ -37,7 +23,6 @@ const Lists = () => {
                 return false;
                 }
         
-                console.log(savedLists[1].movies[0]);
                 setUserData(savedLists);
             } catch (err) {
                 console.error(err);
@@ -51,36 +36,69 @@ const Lists = () => {
         return <h2>LOADING...</h2>;
     }
 
+    const handleDeleteList = async (listID) => {
+        
+        const token = Auth.loggedIn() ? Auth.getToken() : null;
+    
+        if (!token) {
+          return false;
+        }
+    
+        try {
+            removeList({
+                variables: { id: listID }
+            });
+            
+            window.location.replace('/movies')
+        } catch (err) {
+            console.error(err);
+        }
+      };
+
     return(
        <>
       <Jumbotron fluid className='text-light bg-dark'>
         <Container>
-          <h1>Viewing saved lists!</h1>
+          <h1 className="text-center">Viewing saved movies!</h1>
         </Container>
       </Jumbotron>
       <Container>
-        <h2>
+        <h2 className="text-center">
           {userData.length
-            ? `Viewing ${userData.length} saved ${userData.length === 1 ? 'list' : 'lists'}:`
+            ? `Viewing ${userData.length} saved ${userData.length === 1 ? 'movie' : 'movies'}:`
             : 'You have no saved lists!'}
         </h2>
-        <CardColumns>
-          {userData.map((list) => {
-            return (
-              <Card key={list.movies[0].imdbID} border='dark'>
-                {list.movies[0].poster ? <Card.Img src={list.movies[0].poster} alt={`The cover for ${list.movies[0].title}`} variant='top' /> : null}
-                <Card.Body>
-                  <Card.Title>{list.movies[0].title}</Card.Title>
-                  <p className='small'>Actors: {list.movies[0].actors}</p>
-                  <Card.Text>{list.movies[0].plot}</Card.Text>
-                  {/* <Button className='btn-block btn-danger' onClick={() => handleDeleteBook(book.bookId)}>
-                    Delete this Book!
-                  </Button> */}
-                </Card.Body>
-              </Card>
-            );
-          })}
-        </CardColumns>
+        <div className="d-flex container-fluid">
+            <Row className="justify-content-center">
+                
+                {userData.map((list) => {
+                    return (
+                    <Card className="col-lg-3 col-md-5 col-sm-12 list-item m-3" key={list.movies[0].imdbID} border='dark'>
+                        {list.movies[0].poster ? <Card.Img src={list.movies[0].poster} alt={`The cover for ${list.movies[0].title}`} variant='top' /> : null}
+                        <Card.Body className="bg-dark d-flex flex-column justify-content-between">
+                            <Card.Title className="text-info">{list.movies[0].title}</Card.Title>
+                            <p className='small'><strong>Actors: </strong>{list.movies[0].actors}</p>
+                            <Card.Text>{list.movies[0].plot}</Card.Text>
+                            <div className="container-fluid d-flex justify-content-center">
+                                <Button className='mx-2 btn-block btn-danger mt-auto' onClick={() => handleDeleteList(list._id)}>
+                                    Delete this Movie!
+                                </Button>
+                                <Button className='mx-2 btn-block btn-primary mt-auto'>
+                                    <Link
+                                        to={`/movie/${list.movies[0].imdbID}`}
+                                        className="text-light text-decoration-none"
+                                    >
+                                        View this Movie!
+                                    </Link>
+                                </Button>
+                            </div>
+                        </Card.Body>
+                    </Card>
+                    );
+                })}
+            
+            </Row>
+        </div>
       </Container>
     </>
     );
